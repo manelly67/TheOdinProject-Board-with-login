b99d9f113107bba8a26ee99d54c8879cfa924609
manellylopez <manellylopezqqq@gmail.com>
2025-01-27 17:59:09 -0500

    db

--- db/pool.js
+++ db/pool.js
@@ -4,9 +4,11 @@ const myObject = {};
 require('dotenv').config({ processEnv: myObject });
 
 // All of the following properties should be read from environment variables
+// The conexion follow this order first the process.env in deploy to online database
+// second the online database from the local server and third the local database
 
 const connectionString =
-  process.env.DATABASE_URL ||
+  process.env.DATABASE_URL || myObject.DATABASE_URL ||
   `postgresql://${myObject.ROLE_NAME}:${myObject.PASSWORD}@${myObject.HOST}:5432/${myObject.DATABASE}`;
 
 module.exports = new Pool({
--- db/queries.js
+++ db/queries.js
@@ -1,13 +1,97 @@
 const pool = require('./pool');
 
-async function getNameCategory(id) {
-  const { rows } = await pool.query(`SELECT * FROM categories WHERE id=${id}`);
-  const [row] = rows;
-  const { category } = row;
-  return category;
+async function verifyUser(username) {
+  const { rows } = await pool.query('SELECT * FROM users WHERE username = $1', [
+    username,
+  ]);
+  const [user] = rows;
+  return user;
+}
+
+async function verifyMail(email) {
+  const { rows } = await pool.query('SELECT * FROM users WHERE email = $1', [
+    email,
+  ]);
+  const [user] = rows;
+  return user;
+}
+
+async function insertNewUser(dataNewUser) {
+  const user = dataNewUser;
+  const id_membership_status = Number(user.id_membership_status);
+  if (user.username === '') {
+    user.username = user.email;
+  }
+  await pool.query(
+    'INSERT INTO users (firstname, lastname, email, username, user_password, id_membership_status) VALUES ($1, $2, $3, $4, $5, $6)',
+    [
+      user.firstname,
+      user.lastname,
+      user.email,
+      user.username,
+      user.user_password,
+      id_membership_status,
+    ]
+  );
+}
+
+async function updateStatus(arg) {
+  const id = Number(arg);
+  await pool.query(
+    `UPDATE users SET id_membership_status = 2 WHERE id = ${id}`
+  );
+}
+
+async function updateToAdministrator(arg) {
+  const id = Number(arg);
+  await pool.query(
+    `UPDATE users SET id_membership_status = 1 WHERE id = ${id}`
+  );
+}
+
+// this function is called from passport.use()
+async function getUserData(username) {
+  const { rows } = await pool.query('SELECT * FROM users WHERE username = $1', [
+    username,
+  ]);
+  let [user] = rows;
+  return user;
+}
+// this function is called from passport.deserializeUser()
+async function getUserFromId(id) {
+  const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+  const [user] = rows;
+  return user;
+}
+
+async function getAllMessages() {
+  const { rows } = await pool.query(
+    `SELECT messages.id, messages.textm, messages.id_author, messages.added, users.firstname, users.lastname,
+     users.username FROM messages JOIN users ON (messages.id_author=users.id)
+    `
+  );
+  return rows;
+}
+
+async function insertMessage(messageToAdd) {
+  await pool.query(
+    `INSERT INTO messages(textm, id_author, added) VALUES ('${messageToAdd.textm}','${messageToAdd.id_author}','${messageToAdd.added}')`
+  );
+}
+
+async function deleteMessage(id) {
+  await pool.query(`DELETE FROM messages WHERE id=${id}`);
 }
 
 module.exports = {
-    getNameCategory,
-   
-  };
+  verifyUser,
+  verifyMail,
+  insertNewUser,
+  updateStatus,
+  updateToAdministrator,
+  getUserData,
+  getUserFromId,
+  getAllMessages,
+  insertMessage,
+  deleteMessage,
+};

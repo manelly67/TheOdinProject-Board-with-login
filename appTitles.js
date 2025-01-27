d81a679ef218b46f65e22842c232b6204dfde091
manellylopez <manellylopezqqq@gmail.com>
2025-01-27 18:02:34 -0500

    app.js - eslint - prettier

--- appTitles.js
+++ appTitles.js
@@ -1,11 +1,23 @@
 const appObject = {
-    appName: 'Mini Message Board',
-    appShortName: "Mini Board ",
-    title: ['Home', '404 Error', 'All', 'All Messages', 'New Message'],
-    
-  };
-  
+  appName: 'Mini Message CLUB Board',
+  appShortName: 'Mini Board ',
+  title: [
+    'Home',
+    '404 Error',
+    'All',
+    'All Messages',
+    'New Message',
+    'Sign Up',
+    'Welcome',
+    'Login',
+    'Join',
+    'Logout',
+    'Members',
+    'Admin',
+    'Unathorized',
+  ],
+};
+
 module.exports = {
-    appObject,
-  };
-  
+  appObject,
+};
